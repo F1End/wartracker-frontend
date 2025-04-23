7dbe58a8e05ff8f92583b7c88172bc19e7b54a05
F1End <horvirich@gmail.com>
2025-04-23 22:01:03 +0200

    Fixed spacing

--- st_run_local.py
+++ st_run_local.py
@@ -5,9 +5,11 @@ from os import path
 
 from streamlit.web import cli as stcli
 
+
 def main():
     sys.argv = ["streamlit", "run", path.join("main.py")]
     sys.exit(stcli.main())
+
 
 if __name__ == '__main__':
     main()
